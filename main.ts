5713362929ebd5ced91e210249dfe3ac5a0bdc77
nicolehaugen <nicolela@microsoft.com>
2022-03-06 17:58:04 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let timer = 0
+input.onButtonPressed(Button.A, function () {
+    timer = 0
+})
+basic.forever(function () {
+    basic.pause(1000)
+    timer += 1
+    if (timer == 20) {
+    	
+    }
+    if (timer == 30) {
+    	
+    }
+    if (timer == 40) {
+        while (true) {
+        	
+        }
+    }
+})
